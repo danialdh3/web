183e11ffeee4875708e015de17418b3447399a64
danyal <danyalh084@gmail.com>
2023-11-14 16:52:39 +0330

    fm

--- s07.js
+++ s07.js
@@ -1,4 +1,5 @@
 let ht = require("http");
+let fs = require('fs');
 let server = ht.createServer(rh);
 let port = 1800;
 server.listen(port);
@@ -9,9 +10,14 @@ let hedear = {
 
 let route = {
     mor: funcmore,
+    morFile: funcmorFile
 }
 
-function funcmore() {
+function funcmorFile() {
+
+}
+
+function funcmore(response) {
     response.writeHead(200, hedear.html)
     response.write(`
         <html>
@@ -26,6 +32,33 @@ function funcmore() {
 function rh(request, response) {
     let firstpart = request.url.split('/')[1];
     if (firstpart !== 'favicon.ico') {
-        if (route[firstpart] instanceof Function) route[firstpart](response);
+        if (route[firstpart] instanceof Function) {
+            route[firstpart](response);
+        } else {
+            exitBy404Error(response);
+        }
     }
+}
+
+function exitBy404Error(response) {
+    response.writeHead(200, hedear.html)
+    fs.readFile('files/404.html', 'utf-8', (e, d) => {
+        if (e) {
+            exitByError(response);
+        } else {
+            response.write(d);
+            response.end();
+        }
+    });
+}
+
+function exitByError(response) {
+    fs.readFile('files/error.html', 'utf-8', (e, d) => {
+        if (e) {
+            console.log(e);
+        } else {
+            response.write(d);
+            response.end();
+        }
+    });
 }
